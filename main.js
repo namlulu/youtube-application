3c3853dd969fcb6e48e71790fa12f623cb48735c
sean <tmddnr3163@gmail.com>
2022-07-04 21:59:44 +0900

    test: axios 예제 코드 테스트

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow() {
   mainWindow.loadURL('https://music.youtube.com/');
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+const axios = require('axios');
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
@@ -8,5 +11,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type]);
+  }
+
+  try {
+    axios
+      .get('https://my-json-server.typicode.com/zofqofhtltm8015/fs/user')
+      .then((Response) => {
+        console.log(Response.data);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
   }
 });
